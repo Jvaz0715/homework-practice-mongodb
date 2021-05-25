48219a70dd50dd7d7fbf2e1bee89d92bf7ed5b1f
Jvaz0715 <james.vazquez@codeimmersives.com>
2021-05-25 08:22:56 -0400

    brings in recipeController to recipeRouter

--- routes/recipe/recipeRouter.js
+++ routes/recipe/recipeRouter.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+//bring in the recipeController function forumula
+const recipeController = require("./controller/recipeController");
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.json({
