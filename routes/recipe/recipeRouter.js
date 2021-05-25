25ad8cca571fa6fc835034c1f4dddd8a1b443178
Jvaz0715 <james.vazquez@codeimmersives.com>
2021-05-25 08:33:28 -0400

    completes #13

--- routes/recipe/recipeRouter.js
+++ routes/recipe/recipeRouter.js
@@ -4,10 +4,28 @@ var router = express.Router();
 //bring in the recipeController function forumula
 const recipeController = require("./controller/recipeController");
 
-/* GET users listing. */
+/* GET recipes listing. */
 router.get('/', function(req, res, next) {
   res.json({
     test: true,
+  });
+});
+
+// #13 use the getAllRecipe function in recipeRouter.js the function show get all the recipes
+
+router.get("/get-all-recipes", (req, res) => {
+  recipeController.getAllRecipes((err, payload) => {
+    if (err) {
+      res.status(500).json({
+        message: "Error",
+        error: err,
+      });
+    } else {
+      res.json({
+        message: "success",
+        data: payload,
+      });
+    };
   });
 });
 
